23c38b1d8a104899d14e0527c46e2e518536a15d
susufqx <jiangsulirui@gmail.com>
2018-12-12 23:29:22 +0800

    Create folders to store the code and problem text

---
+++ Q001_Two_Sum/q001_two_sum.c
@@ -0,0 +1,14 @@
+int* twoSum(int* nums, int numsSize, int target) {
+    int i, j;
+    int * res = (int *)malloc(sizeof(int) * 2);
+    for (i=0;i<numsSize;i++) {
+        for(j=i+1;j<numsSize;j++) {
+            if(nums[i] + nums[j] == target) {
+               res[0] = i;
+               res[1] = j;
+               return res;
+            }
+        }
+    }
+    return NULL;
+}
