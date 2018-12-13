f55ff6d1f78af187bf61a836c9df88a12000ea24
rui.li <jiangsulirui@gmail.com>
2018-12-13 18:18:04 +0800

    Add problem 014

---
+++ Q014_Longest_Common_Prefix/solution.cpp
@@ -0,0 +1,40 @@
+# include <iostream>
+# include <string>
+# include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+      if(strs.empty()){
+        return "";
+      }
+
+      int len_vector = strs.size();
+      int min_size = strs[0].length();
+      int str_length, i;
+      string common_str = "";
+
+      for(i=1;i<len_vector;i++){
+        str_length = strs[i].length();
+        if(str_length < min_size){
+          min_size = str_length;
+        }
+      }
+      for(i=0;i<min_size;i++){
+        for(int j=1;j<len_vector;j++){
+          if(strs[j-1].substr(0, i+1) != strs[j].substr(0, i+1)){
+            return common_str;
+          }
+        }
+        common_str += strs[0][i];
+      }
+      return common_str;
+    }
+};
+
+int main(int argc, char ** argv){
+  vector<string> s = {};
+  cout<<Solution().longestCommonPrefix(s)<<endl;
+}
