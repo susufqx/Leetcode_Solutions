64a1dcd14ca5ceed568df1d215707eada594d2d2
rui.li <jiangsulirui@gmail.com>
2018-12-14 13:02:43 +0800

    Fix the issue of problem 16

--- Q016_3Sum_Closest/solution.cpp
+++ Q016_3Sum_Closest/solution.cpp
@@ -1,6 +1,6 @@
 # include <iostream>
 # include <vector>
-#include <cstdlib>
+# include <cstdlib>
 
 using namespace std;
 
@@ -8,15 +8,15 @@ class Solution {
 public:
   int threeSumClosest(vector<int>& nums, int target) {
     sort(nums.begin(), nums.end());
-    int abs_delta = abs(target);
-    int delta = target;
+    int delta = nums[0] + nums[1] + nums[3] - target;
+    int abs_delta = abs(delta);
 
     for(int i=0;i<nums.size();i++){
       if(i > 0 && (nums[i] == nums[i-1])) continue;
       int r = i + 1, l = nums.size() - 1;
       int temp = target - nums[i];
 
-      while(r < l){
+      while(r < l){ // 核心就是让 sum 和 temp 不断靠近，让 delta 的绝对值尽可能小
         int sum = nums[r] + nums[l];
         if(sum == temp) return target;
         if(sum > temp){
@@ -33,3 +33,9 @@ public:
     return target + delta;
   }
 };
+
+int main(int argc, char ** argv){
+  vector<int> v = {0, 0, 0};
+  cout<<"The closest sum is : "<<Solution().threeSumClosest(v, 1)<<endl;
+  return 0;
+}
