efae65edd5be5c36342361b6886c0f5b4622ea56
rui.li <jiangsulirui@gmail.com>
2018-12-17 13:46:44 +0800

    Fix README

--- Q024_Swap_Nodes_in_Pairs/solution.cpp
+++ Q024_Swap_Nodes_in_Pairs/solution.cpp
@@ -18,11 +18,11 @@ public:
       ListNode* pre = dummy;
       dummy->next = head; //此处就是获取head的地址保存在dummy->next里, 这也保证了pre指向与dummy指向都是head了
       while (pre->next && pre->next->next) {
-        ListNode* temp = pre->next->next;
-        pre->next->next = temp->next;
-        temp->next = pre->next;
-        pre->next = temp;
-        pre = temp->next;
+        ListNode* temp = pre->next->next; // 第二个节点给temp
+        pre->next->next = temp->next; // 第一个节点指向第三个节点，不再指向第二个
+        temp->next = pre->next; // 第二个节点指向第一个节点，此时已经完成了交换
+        pre->next = temp; // 第一个节点前的节点此刻需要指向第二个节点才对
+        pre = temp->next; // 此时把第三个节点给pre，进行下一轮的操作
       }
       return dummy->next;
     }
