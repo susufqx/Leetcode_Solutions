6c1a2f48ea0233809ad18b4c9fe123865eba102b
rui.li <jiangsulirui@gmail.com>
2018-12-19 11:52:29 +0800

    Finishe problem 25

--- Q025_Reverse_Nodes_in_k-Group/solution.cpp
+++ Q025_Reverse_Nodes_in_k-Group/solution.cpp
@@ -14,7 +14,22 @@ using namespace std;
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-
+      if (!head || k == 1) return head; // 两种特殊情况，第一个是head为NULL，另一种是k为1那就不需要变化
+      ListNode* dumpy = new ListNode(0);
+      ListNode* pre = dumpy;
+      ListNode* cur = head;
+      dumpy->next = head;
+      int i = 0;
+      while (cur) {
+        i++;
+        if (i % k == 0) {
+          pre = reverseOneGroup(pre, cur->next);
+          cur = pre->next;
+        } else {
+          cur = cur->next;
+        }
+      }
+      return dumpy->next;
     }
 
     ListNode* reverseOneGroup(ListNode* pre, ListNode* next) {
@@ -26,6 +41,6 @@ public:
         pre->next = cur;
         cur = last->next;
       }
-      return last;
+      return last; // 此处返回的就是反转完成之后，最后一个节点
     }
 };
